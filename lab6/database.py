f0631d4e01439e3e77a18dfd8c2714d510074662
kalane15 <kalane15@yandex.ru>
2026-05-22 10:25:51 +0300

    fix

--- lab6/database.py
+++ lab6/database.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Замените параметры подключения на свои
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:mysecretpassword@localhost:5431/postgres"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
@@ -10,7 +9,7 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-# Dependency для получения сессии БД
+
 def get_db():
     db = SessionLocal()
     try:
